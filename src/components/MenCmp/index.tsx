a75b3969c8b0236abdfa4af4153067f80f21d652
24Skynet24 <ksk836684@gmail.com>
2024-06-26 20:01:01 +0400

    add Men Component

---
+++ src/components/MenCmp/index.tsx
@@ -0,0 +1,12 @@
+import menImg from "../../assets/img/men.webp"
+import "./men.scss"
+
+const MenCmp = () => {
+    return (
+        <article className="men_cmp">
+            <img src={menImg} alt="Men"/>
+        </article>
+    );
+};
+
+export default MenCmp;
