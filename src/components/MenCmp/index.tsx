feb9cbda10b6389b81ef5d168a5329fd0b62ec41
24Skynet24 <ksk836684@gmail.com>
2024-06-26 20:28:47 +0400

    add mana & health points

--- src/components/MenCmp/index.tsx
+++ src/components/MenCmp/index.tsx
@@ -1,11 +1,30 @@
 // @ts-ignore
 import menImg from "../../assets/img/men.webp"
+import heartImg from "../../assets/img/heart.svg"
+import manaImg from "../../assets/img/mana.svg"
 import "./men.scss"
+
 
 const MenCmp = () => {
     return (
         <article className="men_cmp">
-            <img src={menImg} alt="Men"/>
+            <img src={menImg} alt="Men" className="men_img"/>
+
+            <article className="stats">
+                <div className="stats_item">
+                    <img src={heartImg} alt="HP"/>
+                    <div className="line health">
+                        <span/>
+                    </div>
+                </div>
+
+                <div className="stats_item">
+                    <img src={manaImg} alt="MP"/>
+                    <div className="line mana">
+                        <span/>
+                    </div>
+                </div>
+            </article>
         </article>
     );
 };
