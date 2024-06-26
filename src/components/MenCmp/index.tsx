1ecbac4b16b17260fc52b2c146903a3e8aab4780
24Skynet24 <ksk836684@gmail.com>
2024-06-26 22:21:38 +0400

    add logic men animation

--- src/components/MenCmp/index.tsx
+++ src/components/MenCmp/index.tsx
@@ -5,12 +5,21 @@ import heartImg from "../../assets/img/heart.svg"
 // @ts-ignore
 import manaImg from "../../assets/img/mana.svg"
 import "./men.scss"
+import {useState} from "react";
 
 
 const MenCmp = () => {
+    const [mana, setMana] = useState(90)
+    const [hp, setHp] = useState(0)
+
     const handleClick = (e: any): void => {
         e.target.className = 'men_img men_hover'
         setTimeout(() => {
+            if (mana === 99) {
+                setHp(hp + 1)
+                setMana(0)
+            }
+            else setMana(mana + 1)
             e.target.className = 'men_img'
         }, 100)
     }
@@ -21,22 +30,22 @@ const MenCmp = () => {
 
             <article className="stats">
                 <div className="stats_item">
-                    <img src={heartImg} alt="HP"/>
-                    <div className="line health">
-                        <span/>
+                    <img src={manaImg} alt="MP"/>
+                    <div className="line mana">
+                        <span style={{width: mana + "%"}}/>
                     </div>
                     <span>
-                        100 / 100
+                        {mana} / 100
                     </span>
                 </div>
 
                 <div className="stats_item">
-                    <img src={manaImg} alt="MP"/>
-                    <div className="line mana">
-                        <span/>
+                    <img src={heartImg} alt="HP"/>
+                    <div className="line health">
+                        <span style={{width: hp + "%"}}/>
                     </div>
                     <span>
-                        100 / 100
+                        {hp} / 100
                     </span>
                 </div>
             </article>
