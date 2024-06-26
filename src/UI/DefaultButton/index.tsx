8dfea0f7a319ab2d75c84aed3ee86123c4e50998
24Skynet24 <ksk836684@gmail.com>
2024-06-26 16:19:06 +0400

    add button component

---
+++ src/UI/DefaultButton/index.tsx
@@ -0,0 +1,11 @@
+import "./DefaultButton.scss"
+
+const DefaultButton = (props) => {
+    return (
+        <button {...props}>
+            Toggle
+        </button>
+    );
+};
+
+export default DefaultButton;
